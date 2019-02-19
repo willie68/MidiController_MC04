e6203cc3e709c4bc74bc340e5b039c4814c6c7b6
Klaas, Wilfried <wilfried.klaas@easy.de>
2019-02-19 12:06:49 +0100

    adding encoder to board.

--- 1284_Midi/1284_Midi.cpp
+++ 1284_Midi/1284_Midi.cpp
@@ -1,55 +1,94 @@
 /*
-  Blink
+ Blink
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+ Turns an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+ Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+ it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+ the correct LED pin independent of which board is used.
+ If you want to know what pin the on-board LED is connected to on your Arduino
+ model, check the Technical Specs of your board at:
+ https://www.arduino.cc/en/Main/Products
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
+ modified 8 May 2014
+ by Scott Fitzgerald
+ modified 2 Sep 2016
+ by Arturo Guadalupi
+ modified 8 Sep 2016
+ by Colby Newman
 
-  This example code is in the public domain.
+ This example code is in the public domain.
 
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
+ http://www.arduino.cc/en/Tutorial/Blink
+ */
 #include <Arduino.h>
 #include "LCD_I2C_AIP31068L.h"
-LCD_I2C_AIP31068L lcd(0x7C);
+#include <ClickEncoder.h>
+#include <TimerOne.h>
+
+const byte PIN_ENCODER_A = 4;
+const byte PIN_ENCODER_B = 5;
+const byte PIN_ENCODER_SW = 6;
+
+const byte ENCODER_STP_NOTCH = 4;
+
+LCD_I2C_AIP31068L lcd(0x3E);
+ClickEncoder encoder(PIN_ENCODER_A, PIN_ENCODER_B, PIN_ENCODER_SW, ENCODER_STP_NOTCH);
+
+void checkEncoder();
+
+void timerIsr() {
+	encoder.service();
+}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
+	// set up the LCD's number of columns and rows:
+	lcd.begin(16, 2);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("start");
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(115200);
+	Serial.println("start");
 
-  lcd.print("Hello, Willie!");
+	lcd.print("Hello, Willie!");
+
+	Timer1.initialize(1000);
+	Timer1.attachInterrupt(timerIsr);
+
 }
 
 // the loop function runs over and over again forever
 int i = 0;
+int16_t value = 0;
 void loop() {
-  i++;
-  Serial.print("counter: ");
-  Serial.print(i);
-  Serial.println();
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for a second
+	i++;
+	checkEncoder();
+	Serial.print("counter: ");
+	Serial.print(i);
+	Serial.print(value);
+	Serial.println();
+	if (((millis() / 500) % 2) == 0) {
+		digitalWrite(LED_BUILTIN, HIGH);
+	} else {
+		digitalWrite(LED_BUILTIN, LOW);
+	}
 
-  lcd.setCursor(0, 1);
-  lcd.print("counter ");
-  lcd.print(i);
+	lcd.setCursor(0, 1);
+	lcd.print("counter ");
+	lcd.print(value);
+	ClickEncoder::Button b = encoder.getButton();
+	if (b != ClickEncoder::Open) {
+		if (b == ClickEncoder::Clicked) {
+			value = 0;
+		}
+	}
+}
+
+int16_t last = -1;
+void checkEncoder() {
+	value += encoder.getValue();
+	value = constrain(value, 0, 127);
+	if (value != last) {
+		last = value;
+	}
 }
