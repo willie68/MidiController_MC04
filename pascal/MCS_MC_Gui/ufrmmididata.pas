e6186349de2d8d43d476ede47f96ab3b43e2c3d1
Klaas, Wilfried <wilfried.klaas@easy.de>
2019-04-10 10:54:04 +0200

    next steps defining midi data

--- pascal/MCS_MC_Gui/ufrmmididata.pas
+++ pascal/MCS_MC_Gui/ufrmmididata.pas
@@ -5,7 +5,14 @@ unit ufrmMidiData;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, StdCtrls, Buttons, ComCtrls, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, StdCtrls, Buttons, ComCtrls, ExtCtrls,
+  Spin, uModels;
+
+const
+  MidiCommands: array[0..5] of string = (
+    'Program Change', 'Control Change', 'Next Program', 'Previous Program',
+    'Note On', 'Note Off'
+    );
 
 type
 
@@ -14,14 +21,22 @@ type
   TfrmMidiData = class(TFrame)
     cbMidiCOmmand: TComboBox;
     GroupBox1: TGroupBox;
-    SpeedButton1: TSpeedButton;
+    sbRemove: TSpeedButton;
+    SpinEdit1: TSpinEdit;
+    SpinEdit2: TSpinEdit;
+    SpinEdit3: TSpinEdit;
     TrackBar1: TTrackBar;
     TrackBar2: TTrackBar;
-    procedure SpeedButton1Click(Sender: TObject);
+    procedure cbMidiCOmmandChange(Sender: TObject);
+    procedure sbRemoveClick(Sender: TObject);
+    procedure SpinEditChange(Sender: TObject);
+    procedure TrackBarChange(Sender: TObject);
   private
-
+    FMidiData: TMidiData;
+    procedure SyncMidiData();
   public
-
+    constructor Create(TheOwner: TComponent); override;
+    destructor Destroy; override;
   end;
 
 implementation
@@ -30,12 +45,62 @@ implementation
 
 { TfrmMidiData }
 
-procedure TfrmMidiData.SpeedButton1Click(Sender: TObject);
-var FlowPanel : TFlowPanel;
+procedure TfrmMidiData.sbRemoveClick(Sender: TObject);
+var
+  FlowPanel: TFlowPanel;
 begin
   FlowPanel := Parent as TFLowPanel;
   FlowPanel.RemoveControl(self);
+  self.Free;
+end;
+
+procedure TfrmMidiData.cbMidiCOmmandChange(Sender: TObject);
+begin
+  if (cbMidiCOmmand.ItemIndex = 0) then
+  begin
+    SpinEdit2.Visible := False;
+    TrackBar2.Visible := False;
+  end
+  else if (cbMidiCOmmand.ItemIndex = 0) then
+  begin
+    SpinEdit2.Visible := True;
+    TrackBar2.Visible := True;
+  end;
+end;
+
+procedure TfrmMidiData.SpinEditChange(Sender: TObject);
+begin
+  TrackBar1.Position := SpinEdit1.Value;
+  TrackBar2.Position := SpinEdit2.Value;
+end;
+
+procedure TfrmMidiData.TrackBarChange(Sender: TObject);
+begin
+  SpinEdit1.Value := TrackBar1.Position;
+  SpinEdit2.Value := TrackBar2.Position;
+end;
+
+procedure TfrmMidiData.SyncMidiData();
+begin
+
+end;
+
+constructor TfrmMidiData.Create(TheOwner: TComponent);
+var i : integer;
+begin
+  inherited Create(TheOwner);
+  FMidiData := TMidiData.Create;
+  cbMidiCOmmand.items.clear;
+  for i := 0 to length(MidiCommands)-1 do
+  begin
+    cbMidiCOmmand.items.add(MidiCommands[i]);
+  end;
+end;
+
+destructor TfrmMidiData.Destroy;
+begin
+  FMidiData.Free;
+  inherited Destroy;
 end;
 
 end.
-
